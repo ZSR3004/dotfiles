2c6b00b84e81445e7f4827ee5292aae5faea2915
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-13 13:34:59 -0500

    added gitsigns

--- lua/plugins/navi.lua
+++ lua/plugins/navi.lua
@@ -13,6 +13,29 @@ return {
      { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
      { "<leader>fe", "<cmd>Neotree toggle position=float<cr>", desc = "Neo-tree Float" },
    }
-  }
+  },
 
+
+  { -- GitSigns
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+
+        changedelete = { text = "▎" },
+      },
+    }
+  }
 }
+

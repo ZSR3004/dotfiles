6bc36d9a2a5215eed5b5fdea34caa46a15dd1739
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-15 09:44:17 -0500

    updated keybinds for selecting completion

--- lua/plugins/lang/cmp.lua
+++ lua/plugins/lang/cmp.lua
@@ -63,8 +63,8 @@ return {
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-J>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
@@ -75,7 +75,7 @@ return {
               fallback()
             end
           end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
+          ["<C-K>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then

159c8e5d326cd9e7f7851c36645f91f8bcc614e2
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-15 09:45:16 -0500

    added filetype specification for lazy loading

--- lua/plugins/tex/vimtex.lua
+++ lua/plugins/tex/vimtex.lua
@@ -3,6 +3,7 @@ return {
   { -- Vimtex
     "lervag/vimtex",
     lazy = false,
+    ft = { "tex" },
     init = function()
       vim.g.vimtex_view_method = "zathura"
       vim.g.vimtex_compiler_latexmk = {

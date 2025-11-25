5bf7a05f7b6fe5b82ef4d12e8c9dd0fe72b8bde5
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-25 18:34:47 -0500

    added telescope

--- lua/plugins/navi/navi.lua
+++ lua/plugins/navi/navi.lua
@@ -15,6 +15,18 @@ return {
    }
   },
 
+  { -- Telescope
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fk', ':Telescope keymaps<CR>', { desc = 'Telescope find keybinds' })
+    end
+  },
 
   { -- GitSigns
     "lewis6991/gitsigns.nvim",

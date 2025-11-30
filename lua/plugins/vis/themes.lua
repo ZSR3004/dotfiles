a8464a8aea7e611a5f779f6c51cbec9230f557b9
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-30 18:55:41 -0500

    Added christmas and kanagawa themes

--- lua/plugins/vis/themes.lua
+++ lua/plugins/vis/themes.lua
@@ -4,4 +4,19 @@ return {
     "nyoom-engineering/oxocarbon.nvim"
   },
 
+  { -- Kanagawa
+    "rebelot/kanagawa.nvim",
+    config = function ()
+      require('kanagawa').setup({
+        transparent = true,
+      })
+    end
+  },
+
+  {
+    "chaserensberger/christmas.nvim",
+    name = "christmas",
+    lazy = false,
+  },
+
 }

69005db5891929c015c9f5ce4274f0ab5d4f7c2f
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-30 19:07:26 -0500

    Added let it snow plugin for fun and whimsy

--- lua/plugins/vis/background.lua
+++ lua/plugins/vis/background.lua
@@ -2,6 +2,12 @@ return {
 
   { -- Transparent Background
     "xiyaowong/transparent.nvim",
-  }
+  },
+
+  { -- Let it snow!
+    "marcussimonsen/let-it-snow.nvim",
+    cmd = "LetItSnow",
+    opts = {}
+  },
 
 }

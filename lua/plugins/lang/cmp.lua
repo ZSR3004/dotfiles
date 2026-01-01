49f183ea8b1c56b8bac2f79c315b9dcc310d5dce
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-01 17:27:07 -0600

    Removed friendly-snippets

--- lua/plugins/lang/cmp.lua
+++ lua/plugins/lang/cmp.lua
@@ -1,15 +1,12 @@
 return {
   { -- CMP
     "hrsh7th/nvim-cmp",
-    version = false,
     event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
       "onsails/lspkind.nvim",
     },
     opts = function()

fd9a25991b081c9a038e9c7682ae36b0b7f20bbb
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-02 00:15:28 -0600

    Readded keybinds for moving between buffers

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -3,3 +3,7 @@
 -- File IO and Writing --
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Close buffer" })
+
+-- Buffer Navigation --
+vim.keymap.set("n", "<C-l>", ":bnext<CR>", { desc = "Move to next buffer" })
+vim.keymap.set("n", "<C-l>", ":bprevious<CR>", { desc = "Move to previous buffer" })

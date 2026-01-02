e41d9f0871ef94ccb8a4ba566048ffab75dfafda
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-02 13:22:11 -0600

    Fixed prev buffer keymap from l to h

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -6,8 +6,8 @@ vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Close buffer" })
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape insert mode" })
 
 -- Buffer Navigation --
-vim.keymap.set("n", "<C-l>", ":bnext<CR>", { desc = "Move to next buffer" })
-vim.keymap.set("n", "<C-l>", ":bprevious<CR>", { desc = "Move to previous buffer" })
+vim.keymap.set("n", "<C-l>", ":bnext<CR>", { desc = "Move to next buffer", silent = true })
+vim.keymap.set("n", "<C-h>", ":bprevious<CR>", { desc = "Move to previous buffer", silent = true })
 
 -- Lazy Loader --
 vim.keymap.set("n", "<leader><C-S-l>", ":Lazy<CR>", { desc = "Open Lazy loader interface" })

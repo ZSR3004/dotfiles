59a1f76213c13fa5d217cd46637b93e04a122283
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-02 00:35:07 -0600

    Made vimtex load only for LaTeX files

--- lua/plugins/lang/vimtex.lua
+++ lua/plugins/lang/vimtex.lua
@@ -2,7 +2,6 @@ return {
 
   { -- Vimtex
     "lervag/vimtex",
-    lazy = false,
     ft = { "tex" },
     init = function()
       vim.g.vimtex_view_method = "zathura"
@@ -14,6 +13,7 @@ return {
       vim.keymap.set("n", "<leader>b", "<cmd>VimtexCompile<CR>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>v", "<cmd>VimtexView<CR>", { noremap = true, silent = true })
 
+      -- TODO: Add function to toggle conceal --
       vim.g.vimtex_syntax_conceal = {
         accents = 1,
         ligatures = 1,
@@ -21,15 +21,15 @@ return {
         fancy = 1,
         spacing = 1,
         greek = 1,
-        math_bounds = 0,
+        math_bounds = 1,
         math_delimiters = 1,
         math_fracs = 1,
         math_super_sub = 1,
         math_symbols = 1,
-        sections = 0,
+        sections = 1,
         styles = 1,
       }
-  end,
+    end,
   },
 
 }

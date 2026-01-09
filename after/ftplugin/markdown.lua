febbe5e86210bdde91a86c9f4e9a69872bba07d9
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-09 10:46:27 -0600

    Added color columns

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,9 @@
 vim.opt_local.spell = true
 vim.opt_local.spelllang = { "en_us" }
 
+-- Markview Command --
+-- TODO: Move to plugin config for Markview
 vim.keymap.set("n", "<leader>mc", ":Markview<CR>", { desc = "Open terminal" })
+
+-- Color Column --
+vim.opt_local.colorcolumn = "120"
--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -4,3 +4,6 @@ vim.opt_local.colorcolumn = "80"
 -- Spell Check and Dictionary --
 vim.opt_local.spell = true
 vim.opt_local.spelllang = { "en_us" }
+
+-- Color Column --
+vim.opt_local.colorcolumn = "80"

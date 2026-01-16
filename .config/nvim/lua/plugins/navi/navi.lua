8dc2e4c523704723639d436c50aa1ab192f07f46
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-15 23:10:09 -0600

    Removed tmux related plugins

--- .config/nvim/lua/plugins/navi/navi.lua
+++ .config/nvim/lua/plugins/navi/navi.lua
@@ -55,14 +55,4 @@ return {
     end,
   },
 
-  { -- TMUX Nvim
-    "aserowy/tmux.nvim",
-    opts = {
-      redirect_to_clipboard = true,
-    },
-    config = function(_, opts)
-      require("tmux").setup(opts)
-    end
-  },
-
 }

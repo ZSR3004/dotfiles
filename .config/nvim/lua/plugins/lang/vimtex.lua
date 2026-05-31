ecc5970c8f86285765a3fcdfe570641a5e6153eb
Ziyad Rahman <zrahman3004@gmail.com>
2026-05-31 18:01:25 -0500

    Removed Zathura as primary pdf viewer for Vimtex

--- .config/nvim/lua/plugins/lang/vimtex.lua
+++ .config/nvim/lua/plugins/lang/vimtex.lua
@@ -4,7 +4,7 @@ return {
     "lervag/vimtex",
     ft = { "tex" },
     init = function()
-      vim.g.vimtex_view_method = "zathura"
+      -- vim.g.vimtex_view_method = "zathura"
       vim.g.vimtex_compiler_latexmk = {
         aux_dir = ".build",
       }
